f3c719a8ee46f56ad6ae10e7f0e9041b19a9f845
elspaey <m.elspaey3@gmail.com>
2024-10-15 00:00:29 +0300

    create style &add title to home view

---
+++ lib/Features/home/presentation/views/widgets/featured_listView.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'custom_listView_item.dart';
+
+class featuredBooksListView extends StatelessWidget {
+  const featuredBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * .3,
+        child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.only(right: 15.1, top: 15),
+              child: featuredListViewItem(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,43 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_11/core/assets.dart';
-
+import '../../../../../core/helper_text_widget.dart';
 import 'custom_app_bar.dart';
-import 'custom_listView_item.dart';
+import 'featured_listView.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         customAppBar(),
         featuredBooksListView(),
+        helperTextWidget(
+          hightValueSizedBox: 50,
+          textValue: "Best Seller",
+        )
       ],
-    );
-  }
-}
-
-class featuredBooksListView extends StatelessWidget {
-  const featuredBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 30),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * .3,
-        child: ListView.builder(
-          itemCount: 10,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.only(right: 15.1, top: 15),
-              child: featuredListViewItem(),
-            );
-          },
-        ),
-      ),
     );
   }
 }
