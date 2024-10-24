8b64446a26f8fc33cb41240fd8c9a0d79ded10cc
elspaey <m.elspaey3@gmail.com>
2024-10-25 01:02:02 +0300

    add either class

--- lib/Features/home/domain/repos/home_repos.dart
+++ lib/Features/home/domain/repos/home_repos.dart
@@ -1,7 +1,8 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_application_11/Features/home/domain/entities/book_entity.dart';
+import 'package:flutter_application_11/core/errors/failure.dart';
 
 abstract class homeRepos {
-  Future<List<BookEntity>> fetchFuturedBooks();
-  Future<List<BookEntity>> fetchNewstBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFuturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewstBooks();
 }
-                              
