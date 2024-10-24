1c32b343d455adf109493dff71477c3ddf60dcde
elspaey <m.elspaey3@gmail.com>
2024-10-25 00:36:02 +0300

    book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,12 @@
+class BookEntity {
+  final String image, title, authorName;
+  final num price, rating;
+
+  BookEntity(
+    this.image,
+    this.title,
+    this.authorName,
+    this.price,
+    this.rating,
+  );
+}
