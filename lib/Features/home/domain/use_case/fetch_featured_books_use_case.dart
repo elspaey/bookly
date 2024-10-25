8a13fd009428be9f86b33b7f93502229b4bcffdc
elspaey <m.elspaey3@gmail.com>
2024-10-25 18:07:26 +0300

    refactor to use case class

--- lib/Features/home/domain/use_case/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_case/fetch_featured_books_use_case.dart
@@ -1,14 +1,20 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter_application_11/Features/home/domain/entities/book_entity.dart';
 import 'package:flutter_application_11/Features/home/domain/repos/home_repos.dart';
+import 'package:flutter_application_11/core/errors/failure.dart';
 
-import '../../../../core/errors/failure.dart';
-import '../entities/book_entity.dart';
-
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends useCase<List<BookEntity>, void> {
   final homeRepos homeRepo;
 
   FetchFeaturedBooksUseCase(this.homeRepo);
-  Future<Either<Failure, List<BookEntity>>> fetchFuturedBooks() {
-    return homeRepo.fetchFuturedBooks();
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void param]) async {
+    // TODO: implement call
+    return await homeRepo.fetchFuturedBooks();
   }
+}
+
+abstract class useCase<Type, param> {
+  Future<Either<Failure, Type>> call([param param]);
 }
