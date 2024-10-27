b91c6c96baf053642abdfa53f1bb449f86f52f3b
elspaey <m.elspaey3@gmail.com>
2024-10-28 02:27:02 +0300

    cache featured books

--- lib/Features/home/data/data_scource/home_remote_data_source.dart
+++ lib/Features/home/data/data_scource/home_remote_data_source.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_application_11/Features/home/data/models/book_model/book_model.dart';
 import 'package:flutter_application_11/Features/home/domain/entities/book_entity.dart';
+import 'package:flutter_application_11/core/constants.dart';
 import 'package:flutter_application_11/core/utils/helper%20widgets/api_service.dart';
+
+import '../../../../core/utils/functions/save_books.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> ftchFeatureBooks();
@@ -16,18 +19,18 @@ class homeRemoteDataSourceImpl extends HomeRemoteDataSource {
     var data = await apiService.get(
         endPoint: "volumes?Filtering=free-ebooks&q=programming");
     List<BookEntity> books = getBooksLIst(data);
+    saveBooksData(books, kFeaturedBox);
+
     return books;
   }
 
-
+  @override
   Future<List<BookEntity>> ftchNewestBooks() async {
     var data = await apiService.get(
         endPoint: "volumes?Filtering=free-ebooks&sorting=newest&q=programming");
-    List<BookEntity> books = getBooksLIst(data);    
+    List<BookEntity> books = getBooksLIst(data);
     return books;
   }
-
-
 
   List<BookEntity> getBooksLIst(Map<String, dynamic> data) {
     List<BookEntity> books = [];
